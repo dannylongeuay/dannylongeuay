84f1309723175a40bdff99566afabcf1cf47285c
dannylongeuay <dannylongeuay@yahoo.com>
2021-10-23 20:37:50 -0700

    feat: added timestamp

--- update_readme.py
+++ update_readme.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime, timezone
 from jinja2 import Template
 from requests import get
 
@@ -8,10 +9,11 @@ def main():
 
     response = get("https://quotes.rest/qod?category=inspire")
     qotd = 'Unable to retrieve Quote of the Day.'
+    timestamp = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S %Z')
     if response.status_code == 200:
         qotd = response.json().get('contents').get('quotes')[0].get('quote')
     with open('README.md', 'w') as fp:
-        fp.write(readme_template.render(qotd=qotd))
+        fp.write(readme_template.render(qotd=qotd, timestamp=timestamp))
     logging.info('updated README.md')
 
 if __name__ == "__main__":
